72d67469bd9e4ce7f99f040e553e7c8989d32b6d
Manoj <Manoj@DESKTOP-SC01LHD>
2021-12-14 23:09:48 +0530

    hhggfdfdfhfvg

---
+++ src/gitHubone/Githubone.java
@@ -0,0 +1,16 @@
+package gitHubone;
+
+public class Githubone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int time=20;
+		if (time<18) { 
+		System.out.println("true");
+		}else {
+				System.out.println("false");
+			
+		}
+	}
+
+}
